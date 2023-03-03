52dbd5eaadb5da9d6c1c655b7f5014b97b588517
Norine <norinendoudi@gmail.com>
2023-03-03 19:07:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,60 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Layout from '../components/layout';
+import Link from 'next/link'
+import Layout from '../components/layout'
+import Head from 'next/head'
 
-export default function Home({ allPostsData }) {
+export default function Home() {
+  //regler le padding général et responsive menu hamburger
   return (
-    <div>
+    <Layout>
       <Head>
-        <title>Noca</title>
+        <title>WebTech - Home</title>
+        <meta name="description" content="WebTech home page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Layout>
-
-        <div className='bg-orange-500 relative overflow-hidden block border-b-2 border-black w-full bg-accueil'>
-          <div className='justify-center flex ' id='accueil'>
-            <div className=' mx-12 my-0 min-w-0 max-w-6xl w-full block '>
-              <div className='flex-row flex w-full'>
-                <div className=' text-white w-96 z-10 flex-col h-full pt-24 pb-24 flex dark:text-black'>
-                  <div className=' mb-9 block' id='titre'>
-                    <h1 className="text-7xl font-test font-bold m-4 block tracking-tighter w-72 text-white rounded">
-                      Noca !
-                    </h1>
-                  </div>
-                  <div className=' w-4/5 mb-12 block'>
-                    <h3 className=' text-2xl font-sstitre font-bold text-white rounded'>
-                      Le lieu où vous pouvez tout dire sur vos joueurs préférés (ou détestés) !
-                    </h3>
-
-                  </div>
+      <main>
+        <div className="relative px-6">
+          <div className="mx-auto max-w-4xl">
+            <Link href="/about">
+              <div className="flex justify-center">
+                <div className="relative overflow-hidden rounded-full py-1.5 px-4 leading-6 ring-2 ring-gray-700/10 hover:ring-gray-700/20 dark:ring-white dark:hover:ring-gray-300/20 mt-10">
+                  <span className="text-gray-600 dark:text-white">
+                    Announcing our motivation  {' '}
+                    <a className="font-semibold text-bleu">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </span>
                 </div>
               </div>
-            </div>
+            </Link>
+            <h1 className="mt-16 md:text-5xl text-2xl font-bold tracking-tight text-center ">
+              Presentation of our projects carried out during our studies
+            </h1>
           </div>
-        </div>
-
-        <div className='text-center prose box-content max-w-screen-lg mx-auto px-5'>
-          <h2 className='w-[80%] m-auto py-20 leading-x-10 dark:text-white'>
-            Noca est un blog vous permettant d'entrer dans une communauté de fan du ballon orange,
-            partagez vos ressentis sur les différents joueurs débattez, ayez du fun !
-          </h2>
-
-          <div className="grid mt-7">
-            <div id="carre1" className='flex gap-10  m-auto mb-5'>
-              <Link href="/about">
-                <a className="w-80 shadow-2xl m-auto rounded-lg  h-auto bg-gradient-to-r from-gray-600 to-gray-900 text-center text-white hover:text-orange-500 font-bold">
-                  <p className='text-xl'>About &rarr;</p>
-                  <p>Find in-depth information about the project.</p>
+          <div className="mx-auto max-w-6xl">
+            <p className="mt-14 md:text-md text-sm md:leading-8 leading-4 text-gray-600 text-center dark:text-gray-400 font-bold">
+              Welcome to our website showcasing the computer science projects of our talented students. Here, you will find a variety of innovative and interesting
+              projects that demonstrate the skills and creativity of students. Whether you are a fellow student, a teacher, or just someone with a curiosity about
+              technology, we hope you will find something of interest here. Please take a look around and discover the amazing work of students.
+            </p>
+            <div className="mt-24 flex gap-x-4 justify-center">
+              <Link href='/projects'>
+                <a className="inline-block rounded-lg bg-bleu px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-bleu hover:bg-gray-300 hover:ring-gray-300">
+                  Discover our projects
+                  <span className="text-white" aria-hidden="true">&rarr;</span>
                 </a>
               </Link>
-
-              <Link href="/contacts">
-                <a className="w-80 shadow-2xl rounded-lg h-auto bg-gradient-to-r from-gray-600 to-gray-900 text-center text-white font-bold hover:text-orange-500">
-                  <p className='text-xl'>Contacts &rarr;</p>
-                  <p>Something to tell us?</p>
-                </a>
-              </Link>
-            </div>
-
-            <div id="carre2" className='flex gap-10  m-auto mb-14'>
-              <Link
-                href="/posts">
-                <a className="w-80 m-auto rounded-lg h-auto bg-gradient-to-r from-gray-600 to-gray-900 text-center text-white font-bold hover:text-orange-500">
-                  <p className='text-xl'>Players &rarr;</p>
-                  <p>Discover the players.</p>
-                </a>
-              </Link>
-
-              <Link
-                href="/profile">
-                <a className="w-80 rounded-lg h-auto bg-gradient-to-r from-gray-600 to-gray-900 text-center text-white font-bold hover:text-orange-500">
-                  <p className='text-xl'>Profile &rarr;</p>
-                  <p>
-                    Find your comments and players on your profile
-                  </p>
+              <Link href='/contact'>
+                <a className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-2 ring-gray-900/10 hover:ring-gray-900/30 dark:bg-gray-800 dark:text-white dark:ring-white">
+                  Contact us
+                  <span className="text-black" aria-hidden="true">&rarr;</span>
                 </a>
               </Link>
             </div>
           </div>
         </div>
-        <div className='clear h-10'></div>
-      </Layout>
-
-    </div>
-  );
+      </main>
+    </Layout>
+  )
 }
+
