dbe44ebcf91eee455cd7526fa116dac3612afa54
Norine <norinendoudi@gmail.com>
2023-03-03 19:07:44 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,13 @@ import { Footer } from "./footer"
 
 export default function Layout({ children }) {
   return (
-  <>
-  <Navbar />
-  <div>{children}</div>
-  <Footer />
-  </>
+    <>
+      <div className="flex flex-col h-screen">
+        <Navbar />
+        <main className="py-11 mb-auto bg-slate-200 dark:bg-gray-900">{children}</main>
+        <Footer />
+      </div>
+
+    </>
   )
 }
