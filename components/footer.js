39c2381b8b5cba2fe03780b4b683a096d0019546
Norine <norinendoudi@gmail.com>
2023-03-03 12:45:58 +0100

    Create footer.js
    Footer page

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+export const Footer = () => {
+    return (
+        <footer className='bg-black text-center text-white p-5 w-full text-xs fixed bottom-0'>
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className=""
+            >
+                <strong>Contributors :</strong>&nbsp;Norine Ndoudi, Camille Hascoët
+            </a>
+            <a className='p-5'>© 2022 All rights reserved</a>
+        </footer>
+    );
+}
