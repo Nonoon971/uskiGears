cf47330eacee8682a7025202d230ceaa7604b61f
Norine <norinendoudi@gmail.com>
2023-03-03 19:07:07 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,26 @@
+import Link from "next/link"
+
 export const Footer = () => {
     return (
-        <footer className='bg-black text-center text-white p-5 w-full text-xs fixed bottom-0'>
-            <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className=""
-            >
-                <strong>Contributors :</strong>&nbsp;Norine Ndoudi, Camille Hascoët
-            </a>
-            <a className='p-5'>© 2022 All rights reserved</a>
-        </footer>
+        <div>
+            <div className="mx-auto md:p-4 p-2 bg-bleu flex items-center justify-between dark:bg-gray-800 ">
+                <ul className="flex flex-wrap dark:text-white ">
+                    <li>
+                        <Link href='/about'>
+                            <h3 className="mr-6 hover:text-black text-l invisible md:visible">About</h3>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>
+                            <h3 className="mr-6 hover:text-black text-l invisible md:visible">Contact us</h3>
+                        </Link>
+                    </li>
+                </ul>
+                <span className="  dark:white text-l ">
+                    <a>© 2022 Webtech™. </a>
+                    <a className="md:visible invisible">All Rights Reserved.</a>
+                </span>
+            </div>
+        </div>
     );
 }
